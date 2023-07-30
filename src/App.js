ea1bdc79522f89d25e020ddc0db60d650a35e767
Dritriy Golyshev <4dimariK@gmail.com>
2023-07-30 18:52:03 +0300

    fix(app.js): create main page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { useState } from 'react';
+import './app.css';
+
 function App() {
-  return <></>;
+  const [form, setForm] = useState('singIn');
+
+  const handleFormView = (name) => setForm(name);
+
+  console.log(form);
+
+  return (
+    <main className="container mx-auto">
+      <div className="max-w-[500px] pt-5 mx-auto">
+        <div>
+          <a
+            href="#singUp"
+            onClick={() => handleFormView('singIn')}
+            className="underline text-sky-500 hover:text-sky-800"
+          >
+            SingIn
+          </a>
+          {` / `}
+          <a
+            href="#singUp"
+            onClick={() => handleFormView('singUp')}
+            className="underline text-sky-500 hover:text-sky-800"
+          >
+            SingUp
+          </a>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
