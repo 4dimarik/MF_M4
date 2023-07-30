4392d3f8a95d7a5b573b8591583556e3ca500388
Dritriy Golyshev <4dimariK@gmail.com>
2023-07-30 11:57:32 +0300

    start task 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { useLocalStorage } from './useLocalStorage';
+
 function App() {
-  return <></>;
+  const [token, { setItem, removeItem }] = useLocalStorage('token');
+
+  return (
+    <div>
+      <p>Твой токен: {token}</p>
+      <div>
+        <button onClick={() => setItem('new-token')}>Задать токен</button>
+        <button onClick={() => removeItem()}>Удалить токен</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
