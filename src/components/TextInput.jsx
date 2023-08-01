86166b96a899913cb14c71d5f9fd3bbb34014f5d
Dritriy Golyshev <4dimariK@gmail.com>
2023-08-01 22:09:01 +0300

    fix(TextInput) add prop withAsterisk, icon

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,3 +1,4 @@
+// import React from 'react';
 import PropTypes from 'prop-types';
 
 const sizeList = ['xs', 'sm', 'base', 'lg', 'xl'];
@@ -10,12 +11,12 @@ const textSize = {
   xl: 'text-xl leading-8 [&>.caption]:text-lg leading-7 [&>input]:rounded-xl',
 };
 
-const pSize = {
-  xs: 'px-1 py-1',
-  sm: 'px-2 py-1',
-  base: 'px-3 py-1',
-  lg: 'px-4 py-1',
-  xl: 'px-5 py-1',
+const pxSizes = {
+  xs: 1,
+  sm: 2,
+  base: 3,
+  lg: 4,
+  xl: 5,
 };
 
 const radiusSizes = {
@@ -27,6 +28,7 @@ const radiusSizes = {
 };
 
 function TextInput({
+  type,
   placeholder,
   label,
   description,
@@ -36,34 +38,55 @@ function TextInput({
   size,
   disabled,
   withAsterisk,
+  icon,
 }) {
+  // Динамический класс для радиуса границ (input)
   const radiusSize = radius ? radiusSizes[radius] : '';
+  // Динамический класс для padding по оси x (input)
+  const pxSize = `px-${pxSizes[size]}`;
 
-  const defaultInputClass = `border ${pSize[size]} ${radiusSize}`;
+  // Динамический классы для стилизации (input) вариант Default
+  const defaultInputClass = `w-[100%] align-middle border border-2 py-1 ${pxSize} ${
+    icon && 'pl-7'
+  } ${radiusSize} ${error ? 'border-red-500' : 'border-gray-400'}`;
 
+  // массив классов для вариантов стилизации (input)
   const inputClass = {
-    unstyle: '',
+    unstyle: 'w-[100%]',
     default: defaultInputClass,
     filled: `bg-gray-400 ${defaultInputClass}`,
   };
 
+  // Классы для звездочки
   const asterisk = withAsterisk ? "after:content-['*'] after:ml-0.5" : '';
 
   return (
     <div className={`${textSize[size]}`}>
-      {label && <label className={`font-bold ${asterisk}`}>{label}</label>}
+      {label && (
+        <label className={`block font-bold ${asterisk}`}>{label}</label>
+      )}
       {description && <div className="caption">{description}</div>}
-      <input
-        placeholder={placeholder}
-        className={`${inputClass[variant]} ${error ? 'border-red-500' : ''}`}
-        disabled={disabled}
-      />
+      <div className="relative">
+        {icon && (
+          <div className="flex items-center absolute h-[100%] ml-1">
+            <div className="h-5 w-5 text-gray-400">{icon}</div>
+          </div>
+        )}
+        <input
+          type={type}
+          placeholder={placeholder}
+          className={`${inputClass[variant]} `}
+          disabled={disabled}
+        />
+      </div>
+
       {error && <div className="caption text-red-600">{error}</div>}
     </div>
   );
 }
 
 TextInput.defaultProps = {
+  type: 'text',
   placeholder: '',
   label: null,
   description: null,
@@ -72,9 +95,11 @@ TextInput.defaultProps = {
   disabled: false,
   radius: 'base',
   withAsterisk: false,
+  icon: null,
 };
 
 TextInput.propTypes = {
+  type: PropTypes.string,
   placeholder: PropTypes.string,
   label: PropTypes.string,
   description: PropTypes.string,
@@ -83,6 +108,7 @@ TextInput.propTypes = {
   variant: PropTypes.oneOf(['default', 'filled', 'unstyle']),
   disabled: PropTypes.bool,
   withAsterisk: PropTypes.bool,
+  icon: PropTypes.object,
 };
 
 export default TextInput;
