afbae5f2b1cd708681b25c216e35058b73c23421
Dritriy Golyshev <4dimariK@gmail.com>
2023-08-04 09:09:44 +0300

    fix(component/Nav) add NavLinks

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,28 @@
+import { NavLink } from 'react-router-dom';
+import categories from '../data/categories.json';
+
 export default function Nav() {
-  return <nav>Nav</nav>;
+  const linkClass = ' text-sm font-semibold leading-6';
+  return (
+    <nav
+      className="flex items-center justify-between p-6 lg:px-8"
+      aria-label="Global"
+    >
+      <div className="lg:flex lg:gap-x-12">
+        {Object.keys(categories).map((item) => (
+          <NavLink
+            key={item}
+            to={`/${item}`}
+            className={({ isActive }) =>
+              isActive
+                ? `${linkClass}  text-yellow-500`
+                : `${linkClass} text-gray-900`
+            }
+          >
+            {categories[item]}
+          </NavLink>
+        ))}
+      </div>
+    </nav>
+  );
 }
