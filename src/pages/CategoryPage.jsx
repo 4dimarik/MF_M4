008b548e41b495c913df35949c607da2a90c256e
Dritriy Golyshev <4dimariK@gmail.com>
2023-08-05 13:25:28 +0300

    feat(pages/CategoryPage) add content

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,3 +1,29 @@
+import { useOutletContext } from 'react-router-dom';
+import categories from '../data/categories.json';
+import List from '../components/List';
+
 export default function CategoryPage() {
-  return <div>CategoryPage</div>;
+  const { category, data } = useOutletContext();
+
+  const fields = [
+    {
+      name: 'name',
+      label: 'Name',
+    },
+    {
+      name: 'created',
+      label: 'Created',
+    },
+  ];
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold tracking-tight text-gray-900 text-center">
+        {categories[category]}
+      </h2>
+      <div className="w-[400px] m-auto mt-3">
+        <List data={data} fields={fields} />
+      </div>
+    </div>
+  );
 }
