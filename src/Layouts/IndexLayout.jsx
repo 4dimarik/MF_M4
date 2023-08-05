6684a2549e42b4be343be4c90bdcab667c206bcd
Dritriy Golyshev <4dimariK@gmail.com>
2023-08-05 11:27:27 +0300

    fix(layouts/IndexLayout) import data, check category, add outlet props

--- src/Layouts/IndexLayout.jsx
+++ src/Layouts/IndexLayout.jsx
@@ -1,11 +1,29 @@
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useParams } from 'react-router-dom';
 import Nav from '../components/Nav';
+import categories from '../data/categories.json';
+import characters from '../data/characters.json';
+import episodes from '../data/episode.json';
+import location from '../data/location.json';
+
+const categoriesData = {
+  characters,
+  episodes,
+  location,
+};
 
 export default function IndexLayout() {
+  const { category } = useParams();
+
+  const isValidCategory = Object.keys(categories).includes(category);
+
   return (
     <div className="container mx-auto">
       <Nav />
-      <Outlet />
+      {!isValidCategory ? (
+        <Navigate to="/404" replace={true} />
+      ) : (
+        <Outlet context={{ category, data: categoriesData[category] }} />
+      )}
     </div>
   );
 }
