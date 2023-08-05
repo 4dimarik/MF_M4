b8a415990e75bacabfbfec3ce64c1e875c6ed940
Dritriy Golyshev <4dimariK@gmail.com>
2023-08-05 16:19:18 +0300

    fix(pages/CategoryItemPage): content

--- src/pages/CategoryItemPage.jsx
+++ src/pages/CategoryItemPage.jsx
@@ -1,9 +1,44 @@
-// const categories = {
-//   characters: 'Герои',
-//   episodes: 'Эпизоды',
-//   locations: 'Локации',
-// };
+import { useOutletContext, useParams } from 'react-router-dom';
+import moment from 'moment';
+import ButtonBack from '../components/ButtonBack';
+
+const getValue = (fieldName, value) => {
+  switch (fieldName) {
+    case 'created':
+      return moment(value).format('YYYY-MM-DD HH:mm');
+    default:
+      return value;
+  }
+};
 
 export default function CategoryItemPage() {
-  return <div>CategoryItemPage</div>;
+  const { id } = useParams();
+  const { data } = useOutletContext();
+
+  const obj = data.find((item) => item?.id.toString() === id.toString());
+
+  return (
+    <>
+      <div className="flex">
+        {obj?.image && (
+          <div>
+            <img src={obj.image} />
+          </div>
+        )}
+        <div className="flex-1 p-3">
+          <ul>
+            {Object.keys(obj).map((item) => (
+              <li key={item}>
+                <span className="font-bold uppercase mr-2">{item}:</span>
+                <span>{getValue(item, obj[item])}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="w-100 flex justify-end">
+        <ButtonBack />
+      </div>
+    </>
+  );
 }
