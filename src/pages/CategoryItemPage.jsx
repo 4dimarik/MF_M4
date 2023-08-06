f855334225e132abf852f96fd5f25691e3dc47ed
Dritriy Golyshev <4dimariK@gmail.com>
2023-08-06 19:34:17 +0300

    fix: item info displey

--- src/pages/CategoryItemPage.jsx
+++ src/pages/CategoryItemPage.jsx
@@ -1,11 +1,32 @@
-import { useOutletContext, useParams } from 'react-router-dom';
+import { NavLink, useOutletContext, useParams } from 'react-router-dom';
 import moment from 'moment';
 import ButtonBack from '../components/ButtonBack';
+import { useFetch } from '../hooks/useFetch';
 
-const getValue = (fieldName, value) => {
+const getValue = (fieldName, value, category) => {
   switch (fieldName) {
     case 'created':
       return moment(value).format('YYYY-MM-DD HH:mm');
+    case 'residents':
+    case 'characters':
+    case 'episode':
+      return null;
+    case 'location':
+    case 'origin': {
+      if (value.name !== 'unknown') {
+        const urlParts = value.url.split('/');
+        const to = `/${urlParts[urlParts.length - 2]}/${
+          urlParts[urlParts.length - 1]
+        }`;
+        return (
+          <NavLink to={to} className="underline hover:text-blue-300">
+            {value.name}
+          </NavLink>
+        );
+      } else {
+        return value.name;
+      }
+    }
     default:
       return value;
   }
@@ -13,32 +34,39 @@ const getValue = (fieldName, value) => {
 
 export default function CategoryItemPage() {
   const { id } = useParams();
-  const { data } = useOutletContext();
+  const { category, endpoint } = useOutletContext();
 
-  const obj = data.find((item) => item?.id.toString() === id.toString());
+  const {
+    data: obj,
+    isLoading,
+    error,
+    refetch,
+  } = useFetch('https://rickandmortyapi.com/api/' + endpoint + `/${id}`);
 
   return (
-    <>
-      <div className="flex">
-        {obj?.image && (
-          <div>
-            <img src={obj.image} />
+    !isLoading && (
+      <>
+        <div className="flex">
+          {obj?.image && (
+            <div>
+              <img src={obj.image} />
+            </div>
+          )}
+          <div className="flex-1 p-3">
+            <ul>
+              {Object.keys(obj).map((item) => (
+                <li key={item}>
+                  <span className="font-bold uppercase mr-2">{item}:</span>
+                  <span>{getValue(item, obj[item], category)}</span>
+                </li>
+              ))}
+            </ul>
           </div>
-        )}
-        <div className="flex-1 p-3">
-          <ul>
-            {Object.keys(obj).map((item) => (
-              <li key={item}>
-                <span className="font-bold uppercase mr-2">{item}:</span>
-                <span>{getValue(item, obj[item])}</span>
-              </li>
-            ))}
-          </ul>
         </div>
-      </div>
-      <div className="w-100 flex justify-end">
-        <ButtonBack />
-      </div>
-    </>
+        <div className="w-100 flex justify-end">
+          <ButtonBack />
+        </div>
+      </>
+    )
   );
 }
