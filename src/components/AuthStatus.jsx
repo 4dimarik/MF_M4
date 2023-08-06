5d16649a79926bf21a08e7bd3da5b1bbb82fb574
Dritriy Golyshev <4dimariK@gmail.com>
2023-08-06 10:35:04 +0300

    fix: auth link

--- src/components/AuthStatus.jsx
+++ src/components/AuthStatus.jsx
@@ -9,10 +9,6 @@ export default function AuthStatus() {
   const auth = useAuth();
   const navigate = useNavigate();
 
-  const handleSignIn = () => {
-    console.log('handleSignIn');
-  };
-
   const handleSignOut = () => {
     auth.signout(() => navigate('/'));
   };
@@ -21,11 +17,11 @@ export default function AuthStatus() {
     <>
       {auth.user ? (
         (auth.user,
-        (<ArrowRightOnRectangleIcon className="w-5 h-5 cursor-pointer" />))
+        (<ArrowRightOnRectangleIcon className="w-5 h-5 cursor-pointer" onClick={handleSignOut} />))
       ) : (
         <ArrowLeftOnRectangleIcon
           className="w-5 h-5 hover:cursor-pointer"
-          onClick={handleSignIn}
+          onClick={() => navigate('/login')}
         />
       )}
     </>
